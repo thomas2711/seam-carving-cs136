136602541eabe2f82b1e38712fcc49de7688dec8
thomas2711 <Thomas@Thomass-MacBook-Pro.local>
2015-12-02 14:32:12 -0500

    matplotlib test

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
+import matplotlib.image as mpli
+
 print("this is a test!")
+
+image = mpli.imread("images/example_1.jpg")
+
+for x in image:
+    for i in x:
+        # i = each pixel with rgb values
+        i[0] = 0 # r
+        i[1] = 0 # g
+        i[2] = 100 # b
+
+mpli.imsave("test.jpg", image)
