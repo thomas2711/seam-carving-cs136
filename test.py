577dbc0e56e927bdcded67e30cd60ffd626b1aff
thomas2711 <ter1@williams.edu>
2015-12-07 20:29:06 -0500

    testing

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
-import matplotlib.image as mpli
+from SeamCarving import *
 
 print("this is a test!")
 
-image = mpli.imread("images/example_1.jpg")
+s = SeamCarver("images/example_1.jpg")
+mpli.imsave("test.jpg", s.m_data)
 
-for x in image:
-    for i in x:
-        # i = each pixel with rgb values
-        i[0] = 0 # r
-        i[1] = 0 # g
-        i[2] = 100 # b
-
-
-mpli.imsave("test.jpg", image)
+#image = mpli.imread("images/example_1.jpg")
+#
+#for x in image:
+#    for i in x:
+#        # i = each pixel with rgb values
+#        i[0] = 0 # r
+#        i[1] = 0 # g
+#        i[2] = 100 # b
+#
+#
+#mpli.imsave("test.jpg", image)
